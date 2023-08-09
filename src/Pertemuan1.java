2bf3b19669ee9550ea11c98a4c4c9ad20dc4ed87
MM-450 <lab.petra.mm@gmail.com>
2023-08-09 14:07:47 +0700

    init

---
+++ src/Pertemuan1.java
@@ -0,0 +1,9 @@
+public class Pertemuan1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hi");
+
+        System.out.println("Test");
+    }
+
+}
